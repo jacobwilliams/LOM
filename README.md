2ef0a520c23b6d2855f6f6a12cb42ed843bba2f0
Jacob Williams <jacobwilliams@users.noreply.github.com>
2018-05-03 21:32:34 -0500

    added build files, etc. working on code, not finished.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 LOM tests
+
+### Building
+
+* A [FoBiS](https://github.com/szaghi/FoBiS) configuration file (`lom.fobis`) is also provided that can also build the library and examples. Use the `mode` flag to indicate what to build. For example:
+
+  * To build all the examples using gfortran: `FoBiS.py build -f lom.fobis -mode tests-gnu`
+  * To build all the examples using ifort: `FoBiS.py build -f lom.fobis -mode tests-intel`
+  * To build a static library using gfortran: `FoBiS.py build -f lom.fobis -mode static-gnu`
+  * To build a static library using ifort: `FoBiS.py build -f lom.fobis -mode static-intel`
+
+  The full set of modes are: `static-gnu`, `static-gnu-debug`, `static-intel`, `static-intel-debug`, `shared-gnu`, `shared-gnu-debug`, `shared-intel`, `shared-intel-debug`, `tests-gnu`, `tests-gnu-debug`, `tests-intel`, `tests-intel-debug`
+
+  To generate the documentation using [ford](https://github.com/cmacmackin/ford), run:
+
+```
+  FoBis.py rule --execute makedoc -f lom.fobis
+```
--- src/altitude_maintenance.f90
+++ src/altitude_maintenance.f90
@@ -3,12 +3,41 @@
 !
     module altitude_maintenance_module
 
-    use ddeabm_module
+    use ddeabm_module, only: ddeabm_class,ddeabm_with_event_class
     use fortran_astrodynamics_toolkit
 
     implicit none
 
     private
+
+    character(len=*),parameter :: ephemeris_file = '../eph/JPLEPH_gfortran_mac.421'
+    character(len=*),parameter :: gravfile       = '../grav/gggrx_0020pm_sha.tab'
+
+    type,extends(ddeabm_with_event_class) :: segment
+
+        !! the integrator
+
+        integer :: event = 0 !! event function to use
+
+        real(wp) :: nominal_altitude = 100.0_wp  !! nominal altitude (km)
+        real(wp) :: deadband         = 10.0_wp   !! altitude below nominal to trigger maneuver (km)
+        real(wp) :: r_moon           = 1737.4_wp !! radius of the moon (km)
+
+        type(geopotential_model_pines) :: grav !! central body geopotential model
+        type(jpl_ephemeris)            :: eph  !! the ephemeris
+
+        integer  :: n_eoms = 6            !! size of EOM derivative vector [x,y,z,vx,vy,vz]
+        real(wp) :: integrator_tol = 1.0e-10_wp !! integrator tols
+        integer  :: maxsteps = 10000        !! integrator max steps
+        integer  :: grav_n = 8            !! max grav degree
+        integer  :: grav_m = 8            !! max grav order
+        real(wp) :: root_tol = 1.0e-3_wp  !! event tolerance for deadband (km)
+
+        contains
+
+        procedure,public :: initialize_seg => initialize_segment
+
+    end type segment
 
     public :: altitude_maintenance
 
@@ -17,33 +46,192 @@
 
 !*****************************************************************************************
 !>
+!  Initialize the segment for integration.
+
+    subroutine initialize_segment(me,alt0,deadband_alt)
+
+    implicit none
+
+    class(segment),intent(inout) :: me
+    real(wp),intent(in) :: alt0
+    real(wp),intent(in) :: deadband_alt
+
+    logical :: status_ok
+
+    ! set up the ephemeris:
+    write(*,*) 'loading ephemeris file: '//trim(ephemeris_file)
+    call me%eph%initialize(filename=ephemeris_file,status_ok=status_ok)
+    if (.not. status_ok) error stop 'error initializing ephemeris'
+
+    ! set up the force model [main body is moon]:
+    call me%grav%initialize(gravfile,me%grav_n,me%grav_m,status_ok)
+    if (.not. status_ok) error stop 'error initializing gravity model'
+
+    ! set up the integrator:
+    call me%initialize_event(me%n_eoms,me%maxsteps,ballistic_derivs,&
+                                [me%integrator_tol],[me%integrator_tol],&
+                                 event_func,me%root_tol)
+
+    ! set class variables for event function:
+    me%nominal_altitude = alt0
+    me%deadband = deadband_alt
+
+    end subroutine initialize_segment
+!*****************************************************************************************
+
+!*****************************************************************************************
+!>
 !  Altitude maintenance for a circular lunar orbit - periapsis only control.
 
-    subroutine altitude_maintenance(et0,sma0,inc0,ran0,deadband_alt,dt_max,n_dvs,dv_total,xf)
+    subroutine altitude_maintenance(et0,alt0,inc0,ran0,deadband_alt,dt_max,n_dvs,dv_total,xf)
 
     implicit none
 
     real(wp),intent(in)               :: et0          !! initial ephemeris time (sec)
-    real(wp),intent(in)               :: sma0         !! initial SMA for circular orbit (km)
-    real(wp),intent(in)               :: inc0         !! initial inclination (deg)
-    real(wp),intent(in)               :: ran0         !! initial RAAN (deg)
+    real(wp),intent(in)               :: alt0         !! initial altitude for circular orbit (km)
+    real(wp),intent(in)               :: inc0         !! initial inclination - IAU_MOON of date (deg)
+    real(wp),intent(in)               :: ran0         !! initial RAAN - IAU_MOON of date (deg)
     real(wp),intent(in)               :: deadband_alt !! altitude below initial to trigger periapsis raise (km)
     real(wp),intent(in)               :: dt_max       !! how long to propagate (days)
     integer,intent(out)               :: n_dvs        !! number of DV maneuvers performed
     real(wp),intent(out)              :: dv_total     !! total DV (km/s)
     real(wp),dimension(6),intent(out) :: xf           !! final state - inertial frame (km, km/s)
 
-
-    real(wp) :: etf_maxx  !! max ephemeris time (sec)
+    real(wp),dimension(3) :: r,v
+    real(wp) :: sma          !! circular orbit semi-major axis [km]
+    real(wp) :: etf_max      !! max ephemeris time (sec)
     real(wp) :: min_altitude !! minimum altitude (km)
+    real(wp),dimension(3,3) :: rotmat  !! rotation matrix from ICRF to IAU_MOON
+    real(wp),dimension(3) :: dv !! periapsis raise maneuver [km/s]
+    real(wp),dimension(6) :: x
+    real(wp) :: t
+    real(wp) :: tf
+    integer :: idid
+    real(wp) :: gval
 
+    type(segment) :: seg  !! the integrator
+
+    ! initialize the segment:
+    call seg%initialize_seg(alt0,deadband_alt)
+
+    n_dvs = 0
+    dv_total = zero
+    sma = seg%r_moon + alt0
     etf_max = et0 + dt_max*day2sec
-    min_altitude = sma0-deadband_alt
 
-    ! propagate until the altitude is less than min_altitude
+    ! get initial state in J2000 - Cartesian for integration
+    ! note that inc,ran are in moon-centered-of-date-frame
+    call orbital_elements_to_rv(body_moon%mu,sma,zero,inc0,ran0,zero,zero,r,v)
 
+    ! rotate from body-fixed moon of date to j2000:
+    rotmat = icrf_to_iau_moon(et0)   ! rotation matrix from inertial to body-fixed Moon frame
+    x(1:3) = matmul(transpose(rotmat),r)
+    x(4:6) = matmul(transpose(rotmat),v)  ! because using "of date" iau_moon frame
+
+    ! times are ephemeris time
+    t  = et0
+    tf = etf_max
+    seg%event = 1  ! propagate until the altitude is less than min_altitude
+
+    ! main integration loop:
+    do
+
+        call seg%integrate_to_event(t,x,tf,idid=idid,gval=gval)
+
+        if (idid<0) then
+
+            write(*,'(A,*(I5/))') 'idid: ',idid
+            error stop 'error in integrator'
+
+        elseif (idid==2 .or. idid==3) then
+            ! if we reached the max time, then we are done, so exit
+
+            write(*,*) 'done'
+            exit
+
+        elseif (idid == 1000) then  ! a root has been found
+
+            select case (seg%event)
+            case(1)
+                ! if we hit the min altitude, then integrate to next apoapsis
+                ! and raise the periapsis:
+                seg%event = 2 ! propagate until apoapsis
+
+            case (2)
+
+                ! we have propagated to apoapsis, perform a maneuver to raise periapsis
+
+                ! ... compute DV to raise periapsis back to initial sma
+
+                ! ...
+
+                !call periapsis_apoapsis(body_moon%mu,a,e,rp,ra,vp,va)
+
+               ! dv = ...
+
+                x(4:6) = x(4:6) + dv ! apply the maneuver
+                n_dvs = n_dvs + 1
+                dv_total = dv_total + norm2(dv)
+
+                seg%event = 1     ! continue with normal mode
+                call seg%first_call()  ! have to restart the integration
+                                       ! since we changed the state
+
+            case default
+                error stop 'invalid event value in altitude_maintenance'
+            end select
+
+        else
+            write(*,*) 'unknown exit code from integrator: idid=',idid
+            error stop 'error in altitude_maintenance'
+        end if
+
+    end do
+
+    xf = x
 
     end subroutine altitude_maintenance
+!*****************************************************************************************
+
+!*****************************************************************************************
+!>
+!  Event function: when the altitude drops below the deadband.
+!
+!@note Moon is assumed to be a sphere.
+
+    subroutine event_func(me,t,x,g)
+
+    implicit none
+
+    class(ddeabm_with_event_class),intent(inout) :: me
+    real(wp),intent(in)              :: t  !! time
+    real(wp),dimension(:),intent(in) :: x  !! state -- moon centered inertial frame
+    real(wp),intent(out)             :: g  !! event function
+
+    real(wp) :: alt  !! altitude (km)
+    real(wp) :: p, ecc, inc, raan, aop, tru
+
+    select type (me)
+    class is (segment)
+
+        select case (me%event)
+        case (1)
+            ! offset from deadband altitude
+            alt = norm2(x(1:3)) - me%r_moon
+            g = alt - (me%nominal_altitude - me%deadband)
+        case (2)
+            ! offset from a true anomaly of 180 (apoapsis)
+            call rv_to_orbital_elements(body_moon%mu,x(1:3),x(4:6),p,ecc,inc,raan,aop,tru)
+            if (tru<zero) tru = tru + twopi  ! from 0 -> 360
+            g = tru - pi
+        case default
+            error stop 'invalid event value in event_func'
+        end select
+    class default
+        error stop 'invalid class in event_func'
+    end select
+
+    end subroutine event_func
 !*****************************************************************************************
 
 !*****************************************************************************************
@@ -78,12 +266,12 @@
         v = x(4:6)
 
         ! compute ephemeris time [sec]:
-        et = et_ref + t
+        et = t   ! + et_ref
 
         ! geopotential gravity:
         rotmat = icrf_to_iau_moon(et)   ! rotation matrix from inertial to body-fixed Moon frame
         rb = matmul(rotmat,r)           ! r in body-fixed frame
-        call me%grav%get_acc(rb,grav_n,grav_m,a_geopot)  ! get the acc due to the geopotential
+        call me%grav%get_acc(rb,me%grav_n,me%grav_m,a_geopot)  ! get the acc due to the geopotential
         a_geopot = matmul(transpose(rotmat),a_geopot)    ! convert acc back to inertial frame
 
         ! third-body state vectors (wrt the central body, which is the moon in this case):
@@ -93,8 +281,8 @@
 
         ! third-body perturbation (earth & sun):
         a_third_body = 0.0_wp
-        call third_body_gravity(r,rv_earth_wrt_moon(1:3),mu_earth,a_earth)
-        call third_body_gravity(r,rv_sun_wrt_moon(1:3),mu_sun,a_sun)
+        call third_body_gravity(r,rv_earth_wrt_moon(1:3),body_earth%mu,a_earth)
+        call third_body_gravity(r,rv_sun_wrt_moon(1:3),body_sun%mu,a_sun)
         a_third_body = a_earth + a_sun
 
         !total derivative vector:
@@ -102,13 +290,11 @@
         xdot(4:6) = a_geopot + a_third_body
 
     class default
-
         error stop 'invalid class in ballistic_derivs'
-
     end select
 
     end subroutine ballistic_derivs
 !*****************************************************************************************
 
 
-    end module lom_module
+    end module altitude_maintenance_module
